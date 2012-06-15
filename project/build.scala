36e6b158788a4e5716c90606c3b66fdaee3fdd2e
xuwei-k <6b656e6a69@gmail.com>
2012-06-15 16:47:42 +0900

    build.sbt => build.scala. libraries and sbt versions

---
+++ project/build.scala
@@ -0,0 +1,28 @@
+import sbt._,Keys._
+
+object build extends Build{
+
+  val UF = "0.6.3"
+
+  lazy val root = Project(
+    "githubtree",
+    file(".")
+  ).settings(
+    sbtappengine.Plugin.webSettings ++ Seq(
+      organization := "com.github.xuwei-k",
+      version := "0.1.0-SNAPSHOT",
+      libraryDependencies ++= Seq(
+        "net.databinder" %% "unfiltered-filter" % UF,
+        "net.databinder" %% "unfiltered-spec" % UF % "test",
+        "javax.servlet" % "servlet-api" % "2.3" % "provided",
+        "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container"
+      ),
+      libraryDependencies <+= sbtDependency,
+      resolvers ++= Seq(
+       "jboss" at "https://repository.jboss.org/nexus/content/groups/public/"
+      )
+    ) :_*
+  )
+
+}
+
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,3 @@
-addSbtPlugin("com.eed3si9n" % "sbt-appengine" % "0.4.0")
-
 resolvers ++= Seq(
   "spray repo" at "http://repo.spray.cc",
   Resolver.url("sbt-plugin-releases",
