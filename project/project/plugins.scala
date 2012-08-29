951424ee8f3165d7606b38ba818bfa39b48df6d2
xuwei-k <6b656e6a69@gmail.com>
2012-08-29 15:19:48 +0900

    sbt 0.12.0

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object PluginDef extends Build {
   lazy val root = Project("plugins", file(".")) dependsOn(
-    uri("git://github.com/sbt/sbt-appengine#d2778bd4c3acc8495555a964a92404fb67264fe1")
+    uri("git://github.com/sbt/sbt-appengine#f6b72a97d6e8c1e49a064f1ba37f9b9a9ff6f5d0")
   )
 }
 
