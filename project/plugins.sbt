784a9c54b877e3d62697c88b757fe5f0aa0d59f3
xuwei-k <6b656e6a69@gmail.com>
2012-12-05 10:45:13 +0900

    sbt-appengine

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,6 @@
 resolvers ++= Seq(
   "spray repo" at "http://repo.spray.cc"
 )
+
+addSbtPlugin("com.eed3si9n" % "sbt-appengine" % "0.4.1")
+
--- project/project/plugins.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object PluginDef extends Build {
-  lazy val root = Project("plugins", file(".")) dependsOn(
-    uri("git://github.com/sbt/sbt-appengine#f6b72a97d6e8c1e49a064f1ba37f9b9a9ff6f5d0")
-  )
-}
-
