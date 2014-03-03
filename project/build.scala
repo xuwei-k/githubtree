e3376f4a535bc4ef125a6b4c3b50953c013c9812
xuwei-k <6b656e6a69@gmail.com>
2014-03-04 02:20:17 +0900

    "ghscala-core" % "0.2.5"

--- project/build.scala
+++ project/build.scala
@@ -19,7 +19,7 @@ object build extends Build{
         "org.scalaj"  %% "scalaj-http" % "0.3.6",
         "net.databinder" %% "unfiltered-filter" % UF,
         "net.databinder" %% "unfiltered-spec" % UF % "test",
-//        "com.github.xuwei-k" %% "ghscala" % "0.2.4",
+        "com.github.xuwei-k" %% "ghscala-core" % "0.2.5",
         "javax.servlet" % "servlet-api" % "2.3" % "provided",
         "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container"
       ),
@@ -28,8 +28,6 @@ object build extends Build{
        "jboss" at "https://repository.jboss.org/nexus/content/groups/public/"
       )
     ) :_*
-  ).dependsOn(
-    ProjectRef(uri("git://github.com/xuwei-k/ghscala.git#33c4e2632a"), "core")
   )
 
 }
