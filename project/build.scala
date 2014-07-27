9ffd52fff648a41f0d9bdf488e0b02d6facf0ab4
xuwei-k <6b656e6a69@gmail.com>
2014-07-28 03:51:17 +0900

    update

--- project/build.scala
+++ project/build.scala
@@ -11,12 +11,21 @@ object build extends Build{
       licenses := Seq("MIT License" -> url("http://www.opensource.org/licenses/mit-license.php")),
       version := "0.1.0-SNAPSHOT",
       scalaVersion := "2.10.4",
-      scalacOptions ++= Seq("-deprecation", "-unchecked", "-language:_"),
+      scalacOptions ++= (
+        "-language:postfixOps" ::
+        "-language:implicitConversions" ::
+        "-language:higherKinds" ::
+        "-language:existentials" ::
+        "-deprecation" ::
+        "-unchecked" ::
+        "-Xlint" ::
+        Nil
+      ),
       resolvers += Opts.resolver.sonatypeReleases,
       libraryDependencies ++= Seq(
         "net.databinder" %% "unfiltered-filter" % "0.8.0",
-        "com.github.xuwei-k" %% "ghscala" % "0.2.10",
-        "com.github.xuwei-k" %% "httpz-native" % "0.2.10",
+        "com.github.xuwei-k" %% "ghscala" % "0.2.11",
+        "com.github.xuwei-k" %% "httpz-native" % "0.2.11",
         "javax.servlet" % "servlet-api" % "2.3" % "provided",
         "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container"
       ),
