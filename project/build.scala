af4d4041f3230bf06370aa950fafe249d8259090
xuwei-k <6b656e6a69@gmail.com>
2014-09-14 10:28:34 +0900

    sbt 0.13.6

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ object build extends Build{
       organization := "com.github.xuwei-k",
       licenses := Seq("MIT License" -> url("http://www.opensource.org/licenses/mit-license.php")),
       version := "0.1.0-SNAPSHOT",
-      scalaVersion := "2.10.4",
+      scalaVersion := "2.11.2",
       scalacOptions ++= (
         "-language:postfixOps" ::
         "-language:implicitConversions" ::
@@ -22,15 +22,17 @@ object build extends Build{
         Nil
       ),
       resolvers += Opts.resolver.sonatypeReleases,
-      libraryDependencies ++= Seq(
-        "net.databinder" %% "unfiltered-filter" % "0.8.0",
-        "com.github.xuwei-k" %% "ghscala" % "0.2.13",
-        "com.github.xuwei-k" %% "httpz-native" % "0.2.13",
-        "com.chuusai" %% "shapeless" % "2.0.0" cross CrossVersion.full,
-        "javax.servlet" % "servlet-api" % "2.3" % "provided",
-        "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container"
+      resolvers += Resolver.url("typesafe", url("http://repo.typesafe.com/typesafe/ivy-releases/"))(Resolver.ivyStylePatterns),
+      libraryDependencies ++= (
+        ("net.databinder" %% "unfiltered-filter" % "0.8.1") ::
+        ("com.github.xuwei-k" %% "ghscala" % "0.2.13") ::
+        ("com.github.xuwei-k" %% "httpz-native" % "0.2.13") ::
+        ("com.chuusai" %% "shapeless" % "2.0.0") ::
+        ("javax.servlet" % "servlet-api" % "2.3" % "provided") ::
+        ("org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container") ::
+        ("org.scala-sbt" %% "io" % sbtVersion.value) ::
+        Nil
       ),
-      libraryDependencies <+= sbtDependency,
       resolvers ++= Seq(
        "jboss" at "https://repository.jboss.org/nexus/content/groups/public/"
       )
